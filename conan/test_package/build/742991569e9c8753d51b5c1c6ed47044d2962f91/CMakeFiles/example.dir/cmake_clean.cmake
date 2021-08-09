40b81619b15438887191ce4e68df1c7d1516eb5e
Vijayakumar Mani <vijay.selvimani@gmail.com>
2021-08-09 12:02:18 +0530

    Upload Conan File

---
+++ conan/test_package/build/742991569e9c8753d51b5c1c6ed47044d2962f91/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/example.cpp.obj"
+  "CMakeFiles/example.dir/example.cpp.obj.d"
+  "bin/example.exe"
+  "bin/example.exe.manifest"
+  "bin/example.pdb"
+  "lib/libexample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ conan/test_package/example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "math.h"
+
+int main()
+{
+    double a = 7.4;
+    int b = 99;
+
+    std::cout << "a + b = " <<
+        math::Arithmetic::Add(a, b) << std::endl;
+    std::cout << "a - b = " <<
+        math::Arithmetic::Subtract(a, b) << std::endl;
+    std::cout << "a * b = " <<
+        math::Arithmetic::Multiply(a, b) << std::endl;
+    std::cout << "a / b = " <<
+        math::Arithmetic::Divide(a, b) << std::endl;
+
+    return 0;
+}
